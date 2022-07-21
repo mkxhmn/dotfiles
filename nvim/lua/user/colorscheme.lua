7849533394d8bb3e0060850a1c7bc9443986c85f
Farid <fnikman@gmail.com>
2022-07-21 23:49:35 +0800

    feat: add tokyonight colorscheme

--- nvim/lua/user/colorscheme.lua
+++ nvim/lua/user/colorscheme.lua
@@ -2,6 +2,7 @@ local status_ok, onedark = pcall(require, "onedark")
 if status_ok then
 	onedark.setup({
 		style = "deep",
+		transparent = true,
 	})
 	onedark.load()
 end
@@ -14,6 +15,12 @@ if status_ok_kanagawa then
 		transparent = true, -- do not set background color
 		dimInactive = false, -- dim inactive window `:h hl-NormalNC`
 	})
+end
+
+local status_ok_tokyonight, tokyonight = pcall(require, "tokyonight")
+if status_ok_tokyonight then
+  vim.g.tokyonight_style = "night"
+  vim.g.tokyonight_transparent =true
 end
 
 vim.cmd("colorscheme kanagawa")
