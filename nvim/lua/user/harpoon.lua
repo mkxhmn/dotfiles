72105ceaed6a658c4ea7108cbe6527f473d2bc68
Farid <fnikman@gmail.com>
2022-07-21 23:57:09 +0800

    fix: harpoon keymaps

--- nvim/lua/user/harpoon.lua
+++ nvim/lua/user/harpoon.lua
@@ -2,6 +2,6 @@ local keymap = vim.keymap.set
 local opts = { silent = true, noremap = true }
 
 keymap("n", "<leader>ha", "<cmd>lua require('harpoon.mark').add_file()<CR>", opts)
-keymap("n", "<leader>hh", "<cmd>lua require('harpoon.mark').toggle_quick_menu()<CR>", opts)
-keymap("n", "J", "<cmd>lua require('harpoon.mark').nav_next()<CR>", opts)
-keymap("n", "<leader>hh", "<cmd>lua require('harpoon.mark').nav_prev()<CR>", opts)
+keymap("n", "<leader>hh", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>", opts)
+keymap("n", "J", "<cmd>lua require('harpoon.ui').nav_next()<CR>", opts)
+keymap("n", "F", "<cmd>lua require('harpoon.ui').nav_prev()<CR>", opts)
